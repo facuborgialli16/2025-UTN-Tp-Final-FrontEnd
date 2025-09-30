ae921145de0d4944e276dfd80d72aa7bb8eec44c
facu.dev <facuborgialli16@gmail.com>
2025-09-30 17:57:50 -0300

    first commit

--- src/Components/ContactHeader/ContactHeader.jsx
+++ src/Components/ContactHeader/ContactHeader.jsx
@@ -1,14 +1,20 @@
+// ContactHeader.jsx
 import React from "react";
 import "./ContactHeader.css";
 
 function ContactHeader() {
     return (
-            <header>
-                <div>
+        <header className="contact-header">
+            <div className="header-top">
                 <h2>WhatsApp</h2>
-                </div>
-            </header>
-        
+            </div>
+            <div className="header-search">
+                <input 
+                    type="text" 
+                    placeholder="Buscar o empezar un chat" 
+                />
+            </div>
+        </header>
     );
 }
 
